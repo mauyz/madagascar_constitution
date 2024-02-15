ec92d1b98bbd2862d55366c8d24a6abdb913ef4b
moisefeelin <moise@feelin.be>
2024-02-15 14:04:09 +0300

    update test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:madagascar_constitution/app/app.dart';
 
-import 'package:madagascar_constitution/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
