1162c5007ba22221804ce1b1e206b1870f767931
Mauyz <tsiorymauyz@gmail.com>
2024-02-20 16:53:22 +0300

    edit preamble content page

--- lib/view/pages/preamble_content_page.dart
+++ lib/view/pages/preamble_content_page.dart
@@ -1,8 +1,6 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:madagascar_constitution/model/preamble.dart';
 
-@RoutePage()
 class PreambleContentPage extends StatelessWidget {
   final Preamble preamble;
   const PreambleContentPage({
@@ -12,27 +10,13 @@ class PreambleContentPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            context.router.pop();
-          },
-          icon: const Icon(
-            Icons.arrow_back,
-          ),
-        ),
-        title: Text(preamble.title),
-        elevation: 5,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Text(
-            preamble.content,
-            style: const TextStyle(
-              fontWeight: FontWeight.w500,
-            ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Text(
+          preamble.content,
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
           ),
         ),
       ),
