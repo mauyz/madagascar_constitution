a651f128b3aac357c1fd59aea47d37c160f46e68
Mauyz <tsiorymauyz@gmail.com>
2024-02-20 20:33:28 +0300

    update app name

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"madagascar_constitution", origin, size)) {
+  if (!window.Create(L"Constitution de Madagascar", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
