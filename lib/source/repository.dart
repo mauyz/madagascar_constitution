4c86c389f9e97a7675003a5c727c6dcc4b8014ae
moisefeelin <moise@feelin.be>
2024-02-21 16:23:17 +0300

    add searchText method on Respository

--- lib/source/repository.dart
+++ lib/source/repository.dart
@@ -1,4 +1,5 @@
 import 'package:madagascar_constitution/core/constitution_language.dart';
+import 'package:madagascar_constitution/model/article.dart';
 import 'package:madagascar_constitution/model/constitution.dart';
 import 'package:madagascar_constitution/source/json_parser.dart';
 
@@ -12,5 +13,44 @@ class Repository {
     final constitution = await JsonParser.parseDoc(language);
     _data[language] = constitution;
     return constitution;
+  }
+
+  Future<Map<ConstitutionLanguage, List<Article>>> searchArticles(
+      String text) async {
+    _data[ConstitutionLanguage.mg] ??=
+        await JsonParser.parseDoc(ConstitutionLanguage.mg);
+    final mg = _data[ConstitutionLanguage.mg]!
+        .allArticles
+        .where(
+          (article) => article.content.toLowerCase().contains(
+                text.toLowerCase(),
+              ),
+        )
+        .toList();
+    _data[ConstitutionLanguage.fr] ??=
+        await JsonParser.parseDoc(ConstitutionLanguage.fr);
+    final fr = _data[ConstitutionLanguage.fr]!
+        .allArticles
+        .where(
+          (article) => article.content.toLowerCase().contains(
+                text.toLowerCase(),
+              ),
+        )
+        .toList();
+    _data[ConstitutionLanguage.en] ??=
+        await JsonParser.parseDoc(ConstitutionLanguage.en);
+    final en = _data[ConstitutionLanguage.en]!
+        .allArticles
+        .where(
+          (article) => article.content.toLowerCase().contains(
+                text.toLowerCase(),
+              ),
+        )
+        .toList();
+    return <ConstitutionLanguage, List<Article>>{
+      if (mg.isNotEmpty) ConstitutionLanguage.mg: mg,
+      if (fr.isNotEmpty) ConstitutionLanguage.fr: fr,
+      if (en.isNotEmpty) ConstitutionLanguage.en: en,
+    };
   }
 }
