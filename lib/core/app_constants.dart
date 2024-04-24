d9c017f23918e489e665d54bd78d6f4e35b33e75
Mauyz <tsiorymauyz@gmail.com>
2024-04-24 20:44:15 +0300

    update app constants

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -1,21 +1,27 @@
 class AppConstants {
   const AppConstants._();
 
+  static const baroovEmail = "barouvheritiana@gmail.com";
+  static const mauyzEmail = "tsiorymauyz@gmail.com";
+
   static const appTitle = "Constitution de Madagascar";
-  static const appVersion = "v1.0.3";
+  static const appVersion = "v1.0.4";
+
   static const aboutApp = "Constitution de la 4e République de Madagascar"
       " APRES LE REFERENDUM DU 17 NOVEMBRE 2010,\narrêtés"
       " par la Haute Cour Constitutionnelle le 6 décembre 2010"
       " [version publiée par l'Assemblée Nationale le 7 mai 2014].\n"
       "Source 1: https://www.assemblee-nationale.mg/wp-content/uploads/2019/08/Constitution-4eme-R%C3%A9publique_mg.pdf\n"
       "Source 2: https://tenymalagasy.org/bins/lalampanorenana";
+
   static final copyright = "© Copyright Mauyz, Baroov ${DateTime.now().year}";
-  static const playStoreLink =
-      "https://play.google.com/store/apps/details?id=com.eightgroup.baroov.constitution";
-  static const baroovEmail = "barouvheritiana@gmail.com";
-  static const mauyzEmail = "tsiorymauyz@gmail.com";
-  static const androidId = "com.eightgroup.baroov.constitution";
+
+
+  static const androidId = "com.eightgroup.mauyz.constitution";
   static const iosId = "";
   static const windowsId = "";
   static const macOsId = "";
+
+  static const playStoreLink =
+      "https://play.google.com/store/apps/details?id=$androidId";
 }
