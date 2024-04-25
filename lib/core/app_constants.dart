8e8e76ce1e61272a3689e98369215de61b23fb66
Mauyz <tsiorymauyz@gmail.com>
2024-04-25 09:46:06 +0300

    update app version

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -5,14 +5,12 @@ class AppConstants {
   static const mauyzEmail = "tsiorymauyz@gmail.com";
 
   static const appTitle = "Constitution de Madagascar";
-  static const appVersion = "v1.0.4";
+  static const appVersion = "v1.0.5";
 
   static const aboutApp = "Constitution de la 4e République de Madagascar"
       " APRES LE REFERENDUM DU 17 NOVEMBRE 2010,\narrêtés"
-      " par la Haute Cour Constitutionnelle le 6 décembre 2010"
-      " [version publiée par l'Assemblée Nationale le 7 mai 2014].\n"
-      "Source 1: https://www.assemblee-nationale.mg/wp-content/uploads/2019/08/Constitution-4eme-R%C3%A9publique_mg.pdf\n"
-      "Source 2: https://tenymalagasy.org/bins/lalampanorenana";
+      " par la Haute Cour Constitutionnelle le 6 décembre 2010.\n"
+      "Source: https://tenymalagasy.org/bins/lalampanorenana";
 
   static final copyright = "© Copyright Mauyz, Baroov ${DateTime.now().year}";
 
