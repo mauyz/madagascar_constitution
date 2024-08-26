ef18a0922e07a442d079f7d57b5efd93941ce504
mauyz <tsiorymauyz@gmail.com>
2024-08-26 16:50:13 +0300

    use pathParam on article_content_page.dart

--- lib/view/pages/article_content_page.dart
+++ lib/view/pages/article_content_page.dart
@@ -1,39 +1,80 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:madagascar_constitution/core/constitution_language.dart';
 import 'package:madagascar_constitution/model/article.dart';
+import 'package:madagascar_constitution/source/repository.dart';
+import 'package:madagascar_constitution/view/pages/not_found_page.dart';
+import 'package:provider/provider.dart';
 
 @RoutePage()
 class ArticleContentPage extends StatelessWidget {
-  final Article article;
+  final String language;
+  final int id;
   const ArticleContentPage({
     super.key,
-    required this.article,
+    @pathParam required this.language,
+    @pathParam required this.id,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            context.router.maybePop();
-          },
-          icon: const Icon(
-            Icons.arrow_back,
-          ),
-        ),
-        title: Text(article.title),
-        elevation: 5,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: SelectableText(
-          article.content,
-          style: const TextStyle(
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ),
+    if (!ConstitutionLanguage.values
+        .map(
+          (e) => e.name,
+        )
+        .toList()
+        .contains(language)) {
+      return const NotFoundPage();
+    }
+
+    final languageEnum = switch (language) {
+      "mg" => ConstitutionLanguage.mg,
+      "fr" => ConstitutionLanguage.fr,
+      _ => ConstitutionLanguage.en,
+    };
+    final repo = context.read<Repository>();
+
+    return FutureBuilder<Article?>(
+      future: repo.searchArticleById(languageEnum, id),
+      builder: (_, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final article = snapshot.data;
+          if (article != null) {
+            return Scaffold(
+              appBar: AppBar(
+                elevation: 1,
+                leadingWidth: kIsWeb ? 0.0 : null,
+                leading: kIsWeb
+                    ? const SizedBox.shrink()
+                    : IconButton(
+                        onPressed: () {
+                          context.router.maybePop();
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back,
+                        ),
+                      ),
+                title: Text(article.title),
+              ),
+              body: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: SelectableText(
+                  article.content,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return const NotFoundPage();
+          }
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
