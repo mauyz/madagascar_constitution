e4bf90fd1e169936dc3441b1c5110aa470a78502
mauyz <tsiorymauyz@gmail.com>
2025-02-03 14:11:32 +0300

    fix deprecated android warning

--- android/app/src/main/kotlin/com/eightgroup/mauyz/constitution/MainActivity.kt
+++ android/app/src/main/kotlin/com/eightgroup/mauyz/constitution/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.eightgroup.mauyz.constitution
 
-import android.os.Build
 import android.os.Bundle
-import android.view.WindowInsetsController
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.apply {
-                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_BARS_BY_SWIPE
-            }
-        }
+        WindowCompat.setDecorFitsSystemWindows(window, false) // Recommended method for edge-to-edge display
     }
 }
